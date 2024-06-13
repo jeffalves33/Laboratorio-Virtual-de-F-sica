bc5661638b84cbd558d9b9ecf87a2f83cce21c5c
Jeferson <luiz_cleidi@hotmail.com>
2024-06-12 21:39:57 -0300

    página de simulações

--- simulacoes_page.php
+++ simulacoes_page.php
@@ -5,8 +5,165 @@
 get_header();
 ?>
 
-<div class="container">
-    <h2>Simulações</h2>
-    <p>Aqui está o conteúdo das suas simulações...</p>
-    <!-- Adicione qualquer outro conteúdo aqui -->
-</div>
+<!-- Conteúdo principal -->
+<body>
+    <main>
+        <div class="album">
+            <div class="container">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    <div class="col">
+                        <div class="card shadow-sm bg-transparent border-white">
+                            <div class="ratio ratio-16x9">
+                                <iframe width="359" height="225"
+                                    src="https://www.youtube-nocookie.com/embed/wnhvanMdx4s?si=fS2etgjiJQ_smCBK&amp;controls=0"
+                                    title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text text-white">Titulo do vídeo/simulação</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a href="#" class="btn btn-primary">Acessar</a>
+                                    <small class="text-secondary">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card shadow-sm bg-transparent border-white">
+                            <div class="ratio ratio-16x9">
+                                <iframe width="359" height="225"
+                                    src="https://www.youtube-nocookie.com/embed/wnhvanMdx4s?si=fS2etgjiJQ_smCBK&amp;controls=0"
+                                    title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text text-white">Titulo do vídeo/simulação</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a href="#" class="btn btn-primary">Acessar</a>
+                                    <small class="text-secondary">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card shadow-sm bg-transparent border-white">
+                            <div class="ratio ratio-16x9">
+                                <iframe width="359" height="225"
+                                    src="https://www.youtube-nocookie.com/embed/wnhvanMdx4s?si=fS2etgjiJQ_smCBK&amp;controls=0"
+                                    title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text text-white">Titulo do vídeo/simulação</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a href="#" class="btn btn-primary">Acessar</a>
+                                    <small class="text-secondary">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card shadow-sm bg-transparent border-white">
+                            <div class="ratio ratio-16x9">
+                                <iframe width="359" height="225"
+                                    src="https://www.youtube-nocookie.com/embed/wnhvanMdx4s?si=fS2etgjiJQ_smCBK&amp;controls=0"
+                                    title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text text-white">Titulo do vídeo/simulação</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a href="#" class="btn btn-primary">Acessar</a>
+                                    <small class="text-secondary">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card shadow-sm bg-transparent border-white">
+                            <div class="ratio ratio-16x9">
+                                <iframe width="359" height="225"
+                                    src="https://www.youtube-nocookie.com/embed/wnhvanMdx4s?si=fS2etgjiJQ_smCBK&amp;controls=0"
+                                    title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text text-white">Titulo do vídeo/simulação</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a href="#" class="btn btn-primary">Acessar</a>
+                                    <small class="text-secondary">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card shadow-sm bg-transparent border-white">
+                            <div class="ratio ratio-16x9">
+                                <iframe width="359" height="225"
+                                    src="https://www.youtube-nocookie.com/embed/wnhvanMdx4s?si=fS2etgjiJQ_smCBK&amp;controls=0"
+                                    title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text text-white">Titulo do vídeo/simulação</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a href="#" class="btn btn-primary">Acessar</a>
+                                    <small class="text-secondary">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card shadow-sm bg-transparent border-white">
+                            <div class="ratio ratio-16x9">
+                                <iframe width="359" height="225"
+                                    src="https://www.youtube-nocookie.com/embed/wnhvanMdx4s?si=fS2etgjiJQ_smCBK&amp;controls=0"
+                                    title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text text-white">Titulo do vídeo/simulação</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a href="#" class="btn btn-primary">Acessar</a>
+                                    <small class="text-secondary">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card shadow-sm bg-transparent border-white">
+                            <div class="ratio ratio-16x9">
+                                <iframe width="359" height="225"
+                                    src="https://www.youtube-nocookie.com/embed/wnhvanMdx4s?si=fS2etgjiJQ_smCBK&amp;controls=0"
+                                    title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text text-white">Titulo do vídeo/simulação</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a href="#" class="btn btn-primary">Acessar</a>
+                                    <small class="text-secondary">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card shadow-sm bg-transparent border-white">
+                            <div class="ratio ratio-16x9">
+                                <iframe width="359" height="225"
+                                    src="https://www.youtube-nocookie.com/embed/wnhvanMdx4s?si=fS2etgjiJQ_smCBK&amp;controls=0"
+                                    title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text text-white">Titulo do vídeo/simulação</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a href="#" class="btn btn-primary">Acessar</a>
+                                    <small class="text-secondary">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+<?php
+// Inclui o rodapé do WordPress
+get_footer();
+?>
